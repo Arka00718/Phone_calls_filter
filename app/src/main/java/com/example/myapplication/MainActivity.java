ccb7352cf4180bcbf4ff110d5ce025bf36516456
Arka00718 <arkadij.Glyshov@gmail.com>
2025-05-16 20:12:37 +0500

    Update MainActivity.java

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,32 +1,140 @@
 package com.example.myapplication;
 
+import android.annotation.SuppressLint;
+import android.app.AlarmManager;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.example.myapplication.databinding.ActivityMainBinding;
+import com.google.android.material.timepicker.MaterialTimePicker;
+import com.google.android.material.timepicker.TimeFormat;
+
+import java.util.Calendar;
+
 public class MainActivity extends AppCompatActivity {
+
+    private ActivityMainBinding binding;
+    private MaterialTimePicker  timePicker, timePicker1;
+    private Calendar calendar;
+    private AlarmManager alarmManager;
+    private PendingIntent pendingIntent;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());;
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
-         Button bc = findViewById(R.id.button_to_pep);
-                bc.setOnClickListener(new View.OnClickListener() {
+        setContentView(binding.getRoot());
+        Button bc = findViewById(R.id.button_to_pep);
+        bc.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, Select_calls.class);
+                startActivity(intent);
+            }
+        }); ;
+
+
+
+        createNotificationChannel();
+
+        binding.btSelectTime.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                timePicker = new MaterialTimePicker.Builder()
+                        .setTimeFormat(TimeFormat.CLOCK_12H)
+                        .setHour(12)
+                        .setMinute(0)
+                        .setTitleText("Выберите время начала работы")
+                        .build();
+
+                timePicker.show(getSupportFragmentManager(), "androidknowledge");
+                timePicker.addOnPositiveButtonClickListener(new View.OnClickListener() {
                     @Override
                     public void onClick(View v) {
-                        Intent intent = new Intent(MainActivity.this, Select_calls.class);
-                        startActivity(intent);
+                        if(timePicker.getHour() > 12){
+                            binding.btSelectTime.setText(
+                                    String.format("%02d",(timePicker.getHour() - 12)) + ":" + String.format("%02d", timePicker.getMinute()) + "PM"
+                            );
+                        } else {
+                            binding.btSelectTime.setText(timePicker.getHour() + ":" + timePicker.getMinute() + "AM");
+
+                        }
+
+                        calendar = Calendar.getInstance();
+                        calendar.set(Calendar.HOUR_OF_DAY, timePicker.getHour());
+                        calendar.set(Calendar.MINUTE, timePicker.getMinute());
+                        calendar.set(Calendar.SECOND, 0);
+                        calendar.set(Calendar.MILLISECOND, 0);
+
+
                     }
                 }); ;
 
+            }
+        });
+
+        binding.secondBtSelectTime.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                timePicker = new MaterialTimePicker.Builder()
+                        .setTimeFormat(TimeFormat.CLOCK_12H)
+                        .setHour(12)
+                        .setMinute(0)
+                        .setTitleText("Выберите время начала работы")
+                        .build();
+
+                timePicker.show(getSupportFragmentManager(), "androidknowledge1");
+                timePicker.addOnPositiveButtonClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        if(timePicker.getHour() > 12){
+                            binding.secondBtSelectTime.setText(
+                                    String.format("%02d",(timePicker.getHour() - 12)) + ":" + String.format("%02d", timePicker.getMinute()) + "PM"
+                            );
+                        } else {
+                            binding.secondBtSelectTime.setText(timePicker.getHour() + ":" + timePicker.getMinute() + "AM");
+
+                        }
+
+                        calendar = Calendar.getInstance();
+                        calendar.set(Calendar.HOUR_OF_DAY, timePicker.getHour());
+                        calendar.set(Calendar.MINUTE, timePicker.getMinute());
+                        calendar.set(Calendar.SECOND, 0);
+                        calendar.set(Calendar.MILLISECOND, 0);
+
+
+                    }
+                }); ;
+
+            }
+        });
 
     }
+    private  void createNotificationChannel(){
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.O){
+            CharSequence name = "akchannel";
+            String desc = "Channel for Alarm Manager";
+            int imp = NotificationManager.IMPORTANCE_HIGH;
+            NotificationChannel channel = new NotificationChannel("androidknowlege", name, imp);
+            channel.setDescription(desc);
+
+            NotificationManager notificationManager = getSystemService(NotificationManager.class);
+            notificationManager.createNotificationChannel(channel);
+        }
+    }
+
+
 }
